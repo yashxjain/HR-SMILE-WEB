b12e9d936477465a84e4769d63fa0fdd3ad02bcd
yashxjain <jyash5332@gmail.com>
2024-07-23 18:11:09 +0530

    Login to Web

---
+++ Frontend/src/Login.jsx
@@ -0,0 +1,73 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+// import {Logo} from "../Assets/HRSmileLogo.png"
+const Login = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        axios.post( 'http://localhost:3001/login', {email, password})
+        .then(result => {
+            console.log(result);
+            if(result.data === "Success"){
+                console.log("Login Success");
+                alert('Login successful!')
+                navigate('/home');
+            }
+            else{
+                alert('Incorrect password! Please try again.');
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+
+    return (
+        <div>
+            <div className="d-flex justify-content-center align-items-center text-center vh-100" style= {{backgroundColor : "#6695AF"}}>
+                <div className="bg-white p-3 rounded" style={{width : '40%'}}>
+                    <h2 className='mb-3 primary-col'>Login</h2>
+                    <img src="/images/HRSmileLogo.jpeg" alt="Logo" className="img-fluid" style={{ maxWidth: '150px' }} />
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3 text-start">
+                            <label htmlFor="exampleInputEmail1" className="form-label">
+                                <strong>Email Id</strong>
+                            </label>
+                            <input 
+                                type="email" 
+                                placeholder="Enter Email"
+                                className="form-control" 
+                                id="exampleInputEmail1" 
+                                onChange={(event) => setEmail(event.target.value)}
+                                required
+                            /> 
+                        </div>
+                        <div className="mb-3 text-start">
+                            <label htmlFor="exampleInputPassword1" className="form-label">
+                                <strong>Password</strong>
+                            </label>
+                            <input 
+                                type="password" 
+                                placeholder="Enter Password"
+                                className="form-control" 
+                                id="exampleInputPassword1" 
+                                onChange={(event) => setPassword(event.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    </form>
+                    <Link className='container my-2'>Forget Password</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
